a98320b451c298fb13b5f995cd66c0276b97baa8
TynnanC <87954601+TynnanC@users.noreply.github.com>
2022-12-31 13:31:06 +1100

    Cleaned data using python, imported data, and displayed data.

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+import * as cardData from './data/newData.json';
 
 export default function App() {
+  const [cardId, setCardId] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Testing gitignore</Text>
+      <Text>Name: {cardData[cardId].name}</Text>
+      <Text>Power: {cardData[cardId].power}</Text>
+      <Text>Toughness: {cardData[cardId].toughness}</Text>
+      <Text>Abilities: {cardData[cardId].oracle_text}</Text>
+      <Text>Flavour Text: {cardData[cardId].flavor_text}</Text>
+      <Button title="random card name" onPress={() => setCardId(Math.floor(Math.random() * Object.keys(cardData).length))} />
       <StatusBar style="auto" />
     </View>
   );
