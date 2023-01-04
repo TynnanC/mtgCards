b337ce012df258cb26c565bd2fb38a66e99f6849
TynnanC <87954601+TynnanC@users.noreply.github.com>
2023-01-04 19:28:21 +1100

    Added flexbox styling, start screen below status bar with SafeAreaView.

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { useState } from 'react';
 import * as cardData from './data/newData.json';
+import styles from './styles/homeScreen.js';
 
 export default function App() {
   const [cardId, setCardId] = useState(0);
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Name: {cardData[cardId].name}</Text>
       <Text>Power: {cardData[cardId].power}</Text>
       <Text>Toughness: {cardData[cardId].toughness}</Text>
@@ -14,10 +16,6 @@ export default function App() {
       <Text>Flavour Text: {cardData[cardId].flavor_text}</Text>
       <Button title="random card name" onPress={() => setCardId(Math.floor(Math.random() * Object.keys(cardData).length))} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-
-});
