c80b020a301954a73914a6dbe3c1efe848e62fdc
TynnanC <87954601+TynnanC@users.noreply.github.com>
2023-07-02 19:03:44 +1000

    Removed local data, implement fetch for API calls

--- App.js
+++ App.js
@@ -7,22 +7,43 @@ import styles from './styles/homeScreen.js';
 import { LinearGradient } from 'expo-linear-gradient';
 import BoldText from './components/BoldText.js';
 
+
+
 export default function App() {
-  const [cardId, setCardId] = useState(0);
-  return (
-    <SafeAreaView style={styles.container}>
-      <LinearGradient colors={['rgb(235,159,130)', 'rgb(245,231,185)']} style={styles.subcontainer}>
-        <Image style={styles.image} source={{ uri: cardData[cardId].image_uris.normal }} />
-        <Text><BoldText>Name:</BoldText> {cardData[cardId].name}</Text>
-        <Text><BoldText>Power:</BoldText> {cardData[cardId].power}</Text>
-        <Text><BoldText>Toughness:</BoldText> {cardData[cardId].toughness}</Text>
-        <Text><BoldText>Abilities:</BoldText> {cardData[cardId].oracle_text}</Text>
-        <Text><BoldText>Flavour Text:</BoldText> {cardData[cardId].flavor_text}</Text>
-        <View style={styles.button}>
-          <Button title="random card name" onPress={() => setCardId(Math.floor(Math.random() * Object.keys(cardData).length))} />
-        </View>
-        <StatusBar style="auto" />
-      </LinearGradient>
-    </SafeAreaView>
-  );
+  const [card, setCard] = useState('');
+  const getSingleCard = () => {
+    fetch(`https://api.scryfall.com/cards/random`)
+      .then(response => response.json())
+      .then(data => setCard(data));
+  };
+  if (card != '') {
+    return (
+      <SafeAreaView style={styles.container}>
+        <LinearGradient colors={['rgb(235,159,130)', 'rgb(245,231,185)']} style={styles.subcontainer}>
+          <Image style={styles.image} source={{ uri: card.image_uris.normal }} />
+          <Text><BoldText>Name:</BoldText> {card.name}</Text>
+          <Text><BoldText>Power:</BoldText> {card.power}</Text>
+          <Text><BoldText>Toughness:</BoldText> {card.toughness}</Text>
+          <Text><BoldText>Abilities:</BoldText> {card.oracle_text}</Text>
+          <Text><BoldText>Flavour Text:</BoldText> {card.flavor_text}</Text>
+          <View style={styles.button}>
+            <Button title="Random Card" onPress={getSingleCard} />
+          </View>
+          <StatusBar style="auto" />
+        </LinearGradient>
+      </SafeAreaView>
+    );
+  }
+  else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <LinearGradient colors={['rgb(235,159,130)', 'rgb(245,231,185)']} style={styles.subcontainer}>
+          <View style={styles.button}>
+            <Button title="Random Card" onPress={getSingleCard} />
+          </View>
+          <StatusBar style="auto" />
+        </LinearGradient>
+      </SafeAreaView>
+    );
+  }
 }
