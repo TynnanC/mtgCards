6cd5972a9e1e4ae054e5de7ebd5e7da59fe1e48b
TynnanC <87954601+TynnanC@users.noreply.github.com>
2023-07-04 21:29:32 +1000

    updated styles

--- App.js
+++ App.js
@@ -2,12 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, Image, View, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useState } from 'react';
-import * as cardData from './data/newData.json';
 import styles from './styles/homeScreen.js';
 import { LinearGradient } from 'expo-linear-gradient';
 import BoldText from './components/BoldText.js';
-
-
 
 export default function App() {
   const [card, setCard] = useState('');
@@ -18,32 +15,36 @@ export default function App() {
   };
   if (card != '') {
     return (
-      <SafeAreaView style={styles.container}>
-        <LinearGradient colors={['rgb(235,159,130)', 'rgb(245,231,185)']} style={styles.subcontainer}>
+      <View style={styles.container}>
+        <LinearGradient colors={['rgb(150,150,150)', 'rgb(50,50,50)']} style={styles.subcontainer}>
           <Image style={styles.image} source={{ uri: card.image_uris.normal }} />
-          <Text><BoldText>Name:</BoldText> {card.name}</Text>
-          <Text><BoldText>Power:</BoldText> {card.power}</Text>
-          <Text><BoldText>Toughness:</BoldText> {card.toughness}</Text>
-          <Text><BoldText>Abilities:</BoldText> {card.oracle_text}</Text>
-          <Text><BoldText>Flavour Text:</BoldText> {card.flavor_text}</Text>
-          <View style={styles.button}>
+          <View style={styles.textBox}>
+            <Text><BoldText>Name:</BoldText> {card.name}</Text>
+            <Text><BoldText>Power:</BoldText> {card.power}</Text>
+            <Text><BoldText>Toughness:</BoldText> {card.toughness}</Text>
+            <Text><BoldText>Abilities:</BoldText> {card.oracle_text}</Text>
+            <Text><BoldText>Flavour Text:</BoldText> {card.flavor_text}</Text>
+          </View>
+
+          <View style={styles.randomButton}>
             <Button title="Random Card" onPress={getSingleCard} />
           </View>
           <StatusBar style="auto" />
         </LinearGradient>
-      </SafeAreaView>
+      </View>
     );
   }
   else {
     return (
-      <SafeAreaView style={styles.container}>
-        <LinearGradient colors={['rgb(235,159,130)', 'rgb(245,231,185)']} style={styles.subcontainer}>
-          <View style={styles.button}>
+      <View style={styles.subcontainer}>
+        <LinearGradient colors={['rgb(150,150,150)', 'rgb(50,50,50)']} style={styles.subcontainer}>
+          <View style={styles.initialButton}>
             <Button title="Random Card" onPress={getSingleCard} />
           </View>
           <StatusBar style="auto" />
         </LinearGradient>
-      </SafeAreaView>
+      </View>
+
     );
   }
 }
--- styles/homeScreen.js
+++ styles/homeScreen.js
@@ -10,19 +10,29 @@ const styles = StyleSheet.create({
     subcontainer: {
         flex: 1,
         flexDirection: 'column',
-        alignItems: 'flex-start',
-        margin: 0,
-        padding: '5%',
-        border: 0
+        width: '100%',
+        alignItems: 'flex-start'
     },
     image: {
         //Height is roughly 1.4x the width.
         width: 300,
         height: 420,
+        alignSelf: 'center',
+        borderRadius: 15,
+        marginTop: '15%'
+    },
+    initialButton: {
+        alignSelf: 'center',
+        paddingTop: '50%'
+    },
+    randomButton: {
         alignSelf: 'center'
     },
-    button: {
-        alignSelf: 'center'
+    textBox: {
+        backgroundColor: "#ebebeb",
+        borderRadius: 5,
+        width: '100%',
+        padding: '1%'
     }
 });
 export default styles;
